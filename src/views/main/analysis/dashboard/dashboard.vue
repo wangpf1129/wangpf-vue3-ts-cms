b08cdd1844ba4ddfc68da8e6c5da8264f5521c88
Wangpf <1204981117@qq.com>
2021-10-24 20:51:32 +0800

    feat: init eCharts

--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -1,16 +1,44 @@
 <template>
   <div class="dashboard">
-    <h2>dashboard</h2>
+    <div class="eChartRef" style="width: 600px; height: 600px"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
+import * as eChart from "echarts";
 
 export default defineComponent({
   name: "dashboard",
   setup() {
-    return {};
+    const eChartRef = ref<HTMLElement>();
+    onMounted(() => {
+      const eChartInstance = eChart.init(eChartRef.value!, "light", { renderer: "svg" });
+      const options = {
+        title: {
+          text: "ECharts 入门示例"
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"]
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      };
+      eChartInstance.setOption(options);
+    });
+    return {
+      eChartRef
+    };
   }
 });
 </script>
