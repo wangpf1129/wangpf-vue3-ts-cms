bd3e931d84a7320efa0aa8c6808cb1e5894f9386
Wangpf <1204981117@qq.com>
2021-11-01 11:40:33 +0800

    fix: modiyf base url

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -5,7 +5,7 @@ const isWithCredentials = true;
 if (process.env.NODE_ENV === "development") {
   BASE_URL = "/api";
 } else if (process.env.NODE_ENV === "production") {
-  BASE_URL = "http://152.136.185.210:5000";
+  BASE_URL = "http://152.136.185.210:4000/";
 } else if (process.env.NODE_ENV === "test") {
   BASE_URL = "http://152.136.185.210:5000";
 }
