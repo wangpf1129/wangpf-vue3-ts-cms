ab29fd459721148f56d0fd6d379e7d06d2a5866e
Wangpf <1204981117@qq.com>
2021-11-12 10:31:41 +0800

    test: ci 1.4

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ export default defineComponent({
       console.log(
         `${
           process.env.NODE_ENV === "production" ? "Prod" : "Dev"
-        }  %后台管理 %c FEAT 2021-1111-1412-V1`
+        }  %后台管理 %c FEAT 2021-1112-1031-V1`
       );
     });
     return { zhCn };
